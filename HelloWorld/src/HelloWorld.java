5b99e1262bd31688091ad8ee1e42e5a9b84112bd
ElenaPavlova <hell618lena@gmail.com>
2019-06-27 14:29:10 +0300

    next changes to testing

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Some changes to testing2");
+		System.out.println("And next changes to testing2");
 	}
 }
