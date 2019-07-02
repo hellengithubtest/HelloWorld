2158d7861d1c4f271db0829b2a265ccecd3dae99
ElenaPavlova <hell618lena@gmail.com>
2019-07-02 08:14:49 +0300

    Changes to newbranch

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Some changes to testing2");
 		System.out.println("Changes to proposed branch");
 		System.out.println("Bad changes");
+		System.out.println("Chenges to newbranch");
 	}
 	public static boolean getStatus(){
 	return true;
